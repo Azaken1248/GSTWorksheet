41c416fd0f2fa2e0f2933e777d7f40c2133c6e3f
Azaken <127621783+Azaken1248@users.noreply.github.com>
2024-02-11 17:40:48 +0530

    Update express.js fix perms #3

--- Js/express.js
+++ Js/express.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 // Middleware to enable CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://azaken1248.github.io');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
